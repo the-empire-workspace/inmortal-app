31b3fdfcdf1b629d6990f7390309db34e62c5a05
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-12-15 16:16:12 -0400

    fix test and lint

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,3 @@
-export default '@react-native-async-storage/async-storage/jest/async-storage-mock'
+import AsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+
+export default AsyncStorage
