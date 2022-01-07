7e19dbb2b84fdb8649c6596ddb657010bad99faa
Jorge Bastidas <jorgebastidas9@gmail.com>
2022-01-07 16:18:09 -0400

    IN-37 IN-90 IN-89

---
+++ src/screens/Emergency/elements/EmergencyInformation/index.tsx
@@ -0,0 +1,106 @@
+import React, { FC } from 'react'
+import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
+import { useTheme } from '@providers'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Line from '@assets/img/linea.png'
+import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import Accept from '@assets/img/accept.png'
+import Reject from '@assets/img/reject.png'
+import { Button } from 'components'
+import { translate } from 'utils'
+
+const NewEmergency: FC = () => {
+  const { colors } = useTheme()
+
+  const router: any = useNavigation()
+
+
+  return (
+    <SafeAreaView style={[styles.root, { backgroundColor: colors.background }]}>
+      <View style={styles.buttonContent}>
+        <Text style={[styles.secondaryText, { color: colors.text }]}>Emergencia</Text>
+        <Text style={[styles.title, { color: colors.text }]}>Recaudos</Text>
+        <Image style={styles.line} source={Line} />
+      </View>
+      <ScrollView>
+        <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+          <Text style={[styles.label, { color: colors.text }]}>Certificado de difusion</Text>
+          <View style={styles.buttonContainer}>
+            <Button text='Agregar Certificado' styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.formButton, { backgroundColor: colors.secundary }]} onPress={() => { }} disabled={false} />
+            <Button styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.questionButton, { backgroundColor: colors.secundary }]} text='?' onPress={() => { }} disabled={false} />
+          </View>
+        </View>
+        <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+          <Text style={[styles.label, { color: colors.text }]}>Lugar de difusion</Text>
+          <View style={styles.buttonContainer}>
+            <Button text='Coloque su ubicacion en el mapa' styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.formButton, { backgroundColor: colors.secundary }]} onPress={() => { }} disabled={false} />
+            <Button styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.questionButton, { backgroundColor: colors.secundary }]} text='?' onPress={() => { }} disabled={false} />
+          </View>
+        </View>
+        <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+          <Text style={[styles.label, { color: colors.text }]}>Telefonos Alternos</Text>
+          <View>
+            <TextInput style={{ borderStyle: 'solid', borderWidth: 2, borderColor: colors.text, width: '100%', marginVertical: 10, color: colors.text, padding: 10 }} placeholderTextColor={colors.text} placeholder='000-00-00' />
+            <TextInput style={{ borderStyle: 'solid', borderWidth: 2, borderColor: colors.text, width: '100%', marginVertical: 10, color: colors.text, padding: 10 }} placeholderTextColor={colors.text} placeholder='Opcional' />
+          </View>
+        </View>
+        <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+          <Text style={[styles.label, { color: colors.text }]}>Motivo del deseso</Text>
+          <View style={styles.buttonContainer}>
+            <Button text='Seleccione el motivo de deseso' styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.formButton, { backgroundColor: colors.secundary }]} onPress={() => { }} disabled={false} />
+            <Button styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.questionButton, { backgroundColor: colors.secundary }]} text='?' onPress={() => { }} disabled={false} />
+          </View>
+        </View>
+        <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+          <Text style={[styles.label, { color: colors.text }]}>Validacion de parentesco</Text>
+          <View style={styles.buttonContainer}>
+            <Button text='Agregar documento del fallecido' styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.formButton, { backgroundColor: colors.secundary }]} onPress={() => { }} disabled={false} />
+            <Button styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.questionButton, { backgroundColor: colors.secundary }]} text='?' onPress={() => { }} disabled={false} />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button text='Agregar documento del emisor' styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.formButton, { backgroundColor: colors.secundary }]} onPress={() => { }} disabled={false} />
+            <Button styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.questionButton, { backgroundColor: colors.secundary }]} text='?' onPress={() => { }} disabled={false} />
+          </View>
+        </View>
+        <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+          <Text style={[styles.label, { color: colors.text }]}>Seleccione el tipo de servicio</Text>
+          <View style={styles.buttonContainer}>
+            <Button text='Directo' styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.formButton, { backgroundColor: colors.secundary }]} onPress={() => { }} disabled={false} />
+            <Button styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.questionButton, { backgroundColor: colors.secundary }]} text='?' onPress={() => { }} disabled={false} />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button text='Velacion' styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.formButton, { backgroundColor: colors.secundary }]} onPress={() => { }} disabled={false} />
+            <Button styleText={[styles.secondaryText, { color: colors.secundaryText }]} style={[styles.questionButton, { backgroundColor: colors.secundary }]} text='?' onPress={() => { }} disabled={false} />
+          </View>
+        </View>
+        <View style={styles.buttonContainer2}>
+          <Button
+            text={translate('validate_data')}
+            onPress={() => {
+              router.navigate('EmergencyContact')
+            }}
+            disabled={false}
+            style={[styles.endButton, { backgroundColor: colors.error }]}
+            styleText={[styles.secondaryText, { color: colors.background }]}
+          />
+          <TouchableOpacity
+            style={{ marginVertical: 20 }}
+            onPress={() => {
+              router.navigate('main')
+            }}
+          >
+            <Text
+              style={{ fontSize: 20, fontWeight: 'bold', color: colors.text }}
+            >
+              Cancelar Emergencia
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default NewEmergency
---
+++ src/screens/Emergency/styles.ts
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    width: '100%',
+    elevation: 100,
+    paddingHorizontal: 10,
+  },
+  buttonContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  formButton: {
+    padding: 15,
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 0,
+    elevation: 10,
+  },
+  text: {
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  secondaryText: {
+    fontSize: 17,
+    textAlign: 'center',
+  },
+  listText: {
+    fontSize: 17,
+    textAlign: 'justify',
+    marginVertical: 5,
+  },
+  line: {
+    width: '100%',
+    height: 12,
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+})
