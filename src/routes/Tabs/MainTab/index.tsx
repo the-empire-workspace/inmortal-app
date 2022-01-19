0251df9f136d39a165f1a1894973eefcdf4acd3c
Jorge Bastidas <jorgebastidas9@gmail.com>
2022-01-19 09:21:42 -0400

    update tab

--- src/routes/Tabs/MainTab/index.tsx
+++ src/routes/Tabs/MainTab/index.tsx
@@ -24,7 +24,9 @@ const MainTab: FC<any> = () => {
       tabBar={props => <MainTabComponent {...props} />}
     >
       <Tab.Screen name="Social" component={Social} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Group screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Group>
       <Tab.Screen name="Personal" component={Personal} />
     </Tab.Navigator>
   )
